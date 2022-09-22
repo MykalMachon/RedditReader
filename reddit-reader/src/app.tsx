f25c55decd6b75e104c415f50a4e27701edfcd08
Mykal Machon <mykalmachon@gmail.com>
2022-09-22 22:53:02 +0000

    feat: basic reddit data fetching

--- reddit-reader/src/app.tsx
+++ reddit-reader/src/app.tsx
@@ -1,32 +1,44 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from 'preact/hooks';
+import preactLogo from './assets/preact.svg';
+import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [url, setUrl] = useState();
+  const [data, setData] = useState();
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    // get form data
+    const formData = new FormData(e.target);
+    const { subReddit } = Object.fromEntries(formData);
+
+    // fetch reddit 
+    try {
+      const url = `https://www.reddit.com/${subReddit}/.json`;
+      const res = await fetch(url, { method: 'GET' });
+      const data = await res.json();
+      setData(data);
+    } catch (err) {
+      console.error(err);
+      alert('whoops! something went wrong');
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
+      <h1>Reddit Reader</h1>
+      <p>
+        Reads you stories from reddit; just like the MineCraft videos on TikTok.
       </p>
+      <form action="" onSubmit={submitForm}>
+        <div className="form-control">
+          <label htmlFor="subReddit">SubReddit</label>
+          <input type="text" id="subReddit" name="subReddit" placeholder="r/amitheasshole" required={true} />
+        </div>
+        <button type="submit">Get posts</button>
+      </form>
     </>
-  )
+  );
 }
