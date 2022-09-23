035405c6dccf3339a4a22ffb56b4bebc1025ec84
Mykal Machon <mykal@tinybox.dev>
2022-09-22 23:29:28 -0700

    fix: PWA stuff

--- reddit-reader/vite.config.ts
+++ reddit-reader/vite.config.ts
@@ -4,5 +4,29 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    preact(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.png'],
+      manifest: {
+        name: 'Reddit Reader',
+        short_name: 'RedditReader',
+        description: 'a reddit reader application that reads out reddit posts for you.',
+        theme_color: '#212529',
+        icons: [
+          {
+            src: 'appicon-192-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'appicon-512-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      } 
+    })
+  ],
 });
